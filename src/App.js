a500e5977380399b92f9860b07cd90af3ee1478b
Chandan Das <chandan.das@techmojo.in>
2025-08-31 18:41:56 +0530

    removed files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+
+const ProgressBar = ({ progress }) => {
+  const [animationProgressTime, setAnimationProgressTime] = useState(0);
+
+  useEffect(() => {
+    setTimeout(() => setAnimationProgressTime(progress), 100);
+  }, [progress]);
+  return (
+    <div className="outer">
+      <div
+        className="inner"
+        style={{
+          // width: `${progress}%`,
+          transform: `translateX(${animationProgressTime - 100}%)`,
+          color: progress < 5 ? 'black' : 'white',
+        }}
+        role="progressbar"
+        area-valuenow={progress}
+        area-valuemax="100"
+        area-valuemin="0"
+      >
+        {animationProgressTime}%
+      </div>
+    </div>
+  );
+};
 
 function App() {
+  const percentage = [10, 40, 60, 80];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>Progress Bar</div>
+      {percentage.map((value) => (
+        <ProgressBar key={value} progress={value} />
+      ))}
     </div>
   );
 }
